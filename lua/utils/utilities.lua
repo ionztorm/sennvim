8ecf5c3d6332885870af8eb1467d56f65525c664
ionztorm <leon.codes@icloud.com>
2025-01-03 16:44:34 +0000

    fix lsp issue

--- lua/utils/utilities.lua
+++ lua/utils/utilities.lua
@@ -3,15 +3,35 @@ local M = {}
 
 -- Combine any number of tables into one table
 M.combine_tables = function(...)
-    local combined = {}
-    for _, tbl in ipairs({...}) do
-        if type(tbl) == "table" then
-            for _, value in ipairs(tbl) do
-                table.insert(combined, value)
-            end
-        end
-    end
-    return combined
+	local combined = {}
+	for _, tbl in ipairs({ ... }) do
+		if type(tbl) == "table" then
+			for _, value in ipairs(tbl) do
+				table.insert(combined, value)
+			end
+		end
+	end
+	return combined
+end
+
+M.mason_ensure_installed = function(tools)
+	local mr = require("mason-registry")
+	mr.refresh(function()
+		for _, tool in ipairs(tools) do
+			-- Replace 'ts_ls' with 'tailwind-language-server'
+			if tool == "ts_ls" then
+				tool = "typescript-language-server"
+			end
+			if tool == "tailwindcss" then
+				tool = "tailwind-language-server"
+			end
+
+			local p = mr.get_package(tool)
+			if not p:is_installed() then
+				p:install()
+			end
+		end
+	end)
 end
 
 return M
